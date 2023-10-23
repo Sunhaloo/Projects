0bebe81d0de614619f0506ec1c0195406d2ee5ef
SUNHALOO Shehzaad <81152771+Sunhaloo@users.noreply.github.com>
2023-10-23 20:05:49 +0400

    Create Snakes and Ladders.py
    Simple Random Number Generator which is used in a Snakes and Ladders Game to generate value(s) on die / dice.

---
+++ Simple Codes/Snakes and Ladders.py
@@ -0,0 +1,34 @@
+# Importing "random" Module
+import random
+
+print()
+print("Snakes and Ladder | Ludo | ETC")
+print()
+
+print("1 Die")
+
+# Displaying 1 Random Numbers
+print("Your Number Is: " + str(random.randint(1, 6)))
+
+print()
+print()
+
+print("2 Dice")
+
+# DECLARE num_1: OF STRING
+num_1 = random.randint(1, 6)
+
+# DECLARE num_2: OF STRING
+num_2 = random.randint(1, 6)
+
+# DECLARE total: OF STRING
+total = num_1 + num_2
+
+# Displaying Output
+print("Number 1 : " + str(num_1))
+print("Number 2 : " + str(num_2))
+print("Total: " + str(total))
+
+print()
+
+# S.Sunhaloo
