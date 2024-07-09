54bf3a2461618b3881a85d2be2c5f160cd66a013
Sunhaloo <azmaan151003@gmail.com>
2024-07-09 21:20:41 +0400

    Waiting for friend to git clone then deleting

---
+++ Truck System/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+const users = {
+  user1: 'password1',
+  user2: 'password2',
+  test: 'hello'
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (users[username] && users[username] === password) {
+    req.session.loggedin = true;
+    req.session.username = username;
+    res.redirect('/dashboard');
+  } else {
+    res.send('Incorrect Username and/or Password!');
+  }
+});
+
+app.get('/dashboard', (req, res) => {
+  if (req.session.loggedin) {
+    res.render('dashboard', { username: req.session.username });
+  } else {
+    res.send('Please login to view this page!');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
